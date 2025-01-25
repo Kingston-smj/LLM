1eda04dc305084b155b2380d794ea39571fda3b2
kingston-smj <kingstonsmj@gmail.com>
2025-01-25 03:23:11 +0100

    multi head threading added to LLM_F_S

--- somrthun.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-model_name = "t5-large"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
